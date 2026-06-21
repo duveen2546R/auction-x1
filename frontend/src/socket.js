104be4e6a33db5f5c9c295791376390766460a5f
Duveen Kumar Reddy Rachapalli <159764440+duveen2546R@users.noreply.github.com>
2026-06-22 02:48:31 +0530

    Socket file updated

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,11 +1,16 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL || "http://localhost:5000", {
+const socket = io(
+  import.meta.env.VITE_API_URL || "http://localhost:5000",
+  {
+    path: "/auction/socket.io",
     transports: ["websocket", "polling"],
     secure: true,
     reconnection: true,
     reconnectionAttempts: 5,
-    reconnectionDelay: 1000
-});
+    reconnectionDelay: 1000,
+    withCredentials: true,
+  }
+);
 
 export default socket;
