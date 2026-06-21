db88d4c096ea432b2615b250f78583f08b7c04f1
Duveen Kumar Reddy Rachapalli <159764440+duveen2546R@users.noreply.github.com>
2026-06-22 03:04:02 +0530

    Socket debug 2

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,11 +1,7 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
-console.log("SOCKET DEBUG URL =", URL);
-
-const socket = io(URL, {
-  path: "/socket.io",
+const socket = io("https://savorgo.centralindia.cloudapp.azure.com", {
+  path: "/auction/socket.io",
   transports: ["websocket", "polling"],
   secure: true,
   withCredentials: true,
